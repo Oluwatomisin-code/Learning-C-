06e0ab4e34ba916150c5b5081ea08a5407cdd0d7
Oluwatomisin-code <tomisintomori@gmail.com>
2022-04-06 13:01:20 +0100

    hello world in c#

---
+++ Overview/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello there " + name);
+        }
+    }
+}
